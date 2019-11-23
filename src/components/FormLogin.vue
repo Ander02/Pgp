52853194e9df4d199609e1b90aeb80d3fcb21498
André Pessoa <souza.andre360@gmail.com>
2019-11-23 19:46:44 -0300

    init

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <md-field>
+        <label>Usuário</label>
+        <md-input v-model="user"></md-input>
+    </md-field>
+    <md-field>
+        <label>Senha</label>
+        <md-input v-model="password" type="password"></md-input>
+    </md-field>
+    <md-button class="md-raised md-primary">Entrar</md-button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'FormLogin',
+  data: () => ({
+    'user' : null,
+    'password': null
+  })
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  min-width: 350px;
+  width: 50vw;
+  padding: 50px;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, .5);
+  border-radius: 30px;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <FormLogin />
+</template>
+
+<script>
+// @ is an alias to /src
+import FormLogin from '@/components/FormLogin.vue'
+
+export default {
+  name: 'login',
+  components: {
+    FormLogin
+  }
+}
+</script>
